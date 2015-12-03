df17c9700a7b98c3eb12af7b638c6cc0a5c5684c
ALeonard9 <aleonard9@hotmail.com>
2015-12-02 21:47:29 -0500

    Initial push

---
+++ cgi-bin/smash/deleteGame.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+include '../connectToDB.php';
+
+$gameID = $_GET['gameID'];
+
+$sql = "DELETE FROM `smash`.`game` WHERE `game_id`='". $gameID ."'";
+echo $sql;
+
+if ($_SESSION['username'])
+	{
+		$db->exec($sql);
+		// header("Location: gameRecords.php");
+		exit;
+	}
+else
+	{
+	die("You must login");
+	}
+?>
