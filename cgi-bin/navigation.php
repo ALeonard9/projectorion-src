8769d3d58e1fe99823eb59354513801be539ad30
ALeonard9 <aleonard9@hotmail.com>
2015-12-05 17:49:33 -0500

    Adding resume section

--- cgi-bin/navigation.php
+++ cgi-bin/navigation.php
@@ -29,6 +29,12 @@
             <li><a href="../videogame/videogame.php">Video Games</a></li>
           </ul>
         </li>
+        <li class='dropdown'>
+          <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>About <span class='caret'></span></a>
+          <ul class='dropdown-menu'>
+            <li><a href='http://resume.aleonard.us'>Resume</a></li>
+          </ul>
+        </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
 <?php  if ($_SESSION['username'])
