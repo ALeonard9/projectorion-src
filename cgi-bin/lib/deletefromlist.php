08a7c151fb1a6bf09bc7bd2fbdd18369e548819b
ALeonard9 <aleonard9@hotmail.com>
2021-07-31 22:25:06 -0400

    Fixed remove from list

--- cgi-bin/lib/deletefromlist.php
+++ cgi-bin/lib/deletefromlist.php
@@ -16,7 +16,7 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_SESSION['userid']))
 	{
 		try {
-			$stmt = ("DELETE FROM $table WHERE `g_id`=:id AND `user_id`=:user");
+			$stmt = $db->prepare("DELETE FROM $table WHERE `g_id`=:id AND `user_id`=:user");
 			$stmt->bindParam(':user', $user_id);
 			$stmt->bindParam(':id', $id);
 			$stmt->execute();
